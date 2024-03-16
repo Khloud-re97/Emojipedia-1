cdae79810de83567e55544e0a4facd6287410239
Kheloud Saeed Abdelletif <khloud.saeed.ce97@gmail.com>
2024-03-16 19:49:12 +0300

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,32 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🍓",
+    name: "Strawberry",
+    meaning:
+      "The fruit of a rich red strawberry dotted with seeds and crowned with green leaves. Google previously depicted the fruit in orange."
+  },
+  {
+    id: 2,
+    emoji: "🌸",
+    name: "Cherry Blossom",
+    meaning:
+      "The pink flower of a cherry blossom tree. Depicted as a single, light-pink cherry blossom with five, notched petals and red-tipped stamens in a yellow or white center. A traditional symbol in Japan, where the flower is called sakura and celebrated in annual viewings. Commonly used for Valentine’s Day, Mother’s Day, and other special occasions. May be more generally used to express such ideas as love and beauty. Also used as a pink accent color."
+  },
+  {
+    id: 3,
+    emoji: "🩷",
+    name: "Pink Heart",
+    meaning:
+      "A plain pink heart, expected to be frequently used alongside other hearts. Used to represent love, friendship, and feelings of warmth, and the color pink."
+  },
+  {
+    id: 4,
+    emoji: "🫀",
+    name: "Anatomical Heart",
+    meaning:
+      "A red anatomical heart, shown with blue or purple veins on some platforms. Sometimes used interchangeably with ❤️ Red Heart."
+  },
+];
+
+export default emojipedia;
